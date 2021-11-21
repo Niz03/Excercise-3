4da80d016cf02094b6bfdd00ee614e80dc903823
Nizar Arjoun <Nizar Arjoun@DESKTOP-GN2K77D>
2021-11-21 10:49:35 +0800

    Add project files.

---
+++ Excercise_3.py
@@ -0,0 +1,13 @@
+# Exercise 2: Write a program that calculates and prints the number of minutes in a year.
+
+# Variables are declared
+hour = 60
+day = 24
+year = 365
+
+# Basic multiplication, minutes in an hour, then in day, then in year.
+totalMinutes = hour * day * year
+
+# Final Output
+print ("Hour = 60 Minutes\nDay = 24 Hours\nYear = 365 Days")
+print("There are " + str(totalMinutes) + " in a year.")
